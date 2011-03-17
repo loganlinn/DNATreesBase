2539bde9a668c52ab4414b210293606b48db9f99
Logan Linn <logan.linn@gmail.com>
2011-03-17 18:03:39 -0400

    new tree starts with empty node

--- src/Tree.java
+++ src/Tree.java
@@ -4,83 +4,33 @@ import java.util.Queue;
 /**
  * 
  * @author loganlinn
- *
+ * 
  */
-public class Tree{
-	private Node root = null;	// Root of tree
-	
+public class Tree {
+	private Node root = null; // Root of tree
+
 	/**
-	 * Constructs a Tree object given a sequence. 
-	 * The first node in the tree is always a SequenceNode
+	 * Construct a Tree with an EmptyLeafNode (flyweight) as the root Node.
 	 * 
 	 * @param sequenceDescriptor
 	 */
-	public Tree(String sequenceDescriptor){
-		createRoot(sequenceDescriptor);
+	public Tree() {
+		root = EmptyLeafNode.getInstance();
 	}
-	
-	private void createRoot(String sequenceDescriptor){
-		root = new SequenceLeafNode(new ArraySequence(sequenceDescriptor));
-	}
-	
+
 	/**
 	 * Executes a queue of Operation objects
+	 * 
 	 * @param operations
 	 */
-	public void executeOperations(Queue<Command> operations){
+	public void executeOperations(Queue<Command> operations) {
 		// Require that operations isn't null and has Operations
-		if(operations == null || operations.size() < 1){
+		if (operations == null || operations.size() < 1) {
 			return;
 		}
 		// Iterate over all operations and call the execute method
-		for(Command operation : operations){
+		for (Command operation : operations) {
 			root = operation.execute(root);
 		}
-	}
-	
-	/**
-	 * Sample test: http://courses.cs.vt.edu/~cs3114/Spring11/P2sampleinputTree.pdf
-	 */
-	public static void main(String[] args) {	
-//		Tree dnaTree = new Tree("AAAA");
-//		
-//		Queue<Operation> sampleOperations = new LinkedList<Operation>();
-//		
-//		sampleOperations.add(new InsertOperation("ACGT"));
-////		sampleOperations.add(new PrintOperation());
-//		
-//		sampleOperations.add(new InsertOperation("AA"));
-////		sampleOperations.add(new PrintOperation());
-//		
-//		sampleOperations.add(new InsertOperation("AAACCCCGGTGAAAACGTA"));
-////		sampleOperations.add(new PrintOperation());
-//		
-//		sampleOperations.add(new InsertOperation("ACTGGGAA"));
-////		sampleOperations.add(new PrintOperation());
-//		
-//		sampleOperations.add(new RemoveOperation("ACGT"));
-////		sampleOperations.add(new PrintOperation());
-//		
-//		sampleOperations.add(new InsertOperation("ACCTT"));		
-////		sampleOperations.add(new PrintOperation());
-//		
-//		sampleOperations.add(new InsertOperation("ACTTA"));
-////		sampleOperations.add(new PrintOperation());
-//		
-//		sampleOperations.add(new InsertOperation("TATA"));
-////		sampleOperations.add(new PrintOperation());		
-//		
-//		sampleOperations.add(new InsertOperation("TCG"));
-////		sampleOperations.add(new PrintOperation());
-//		
-////		sampleOperations.add(new PrintLengthsOperation());
-//		sampleOperations.add(new PrintStatsOperation());
-//		
-//		sampleOperations.add(new ExactSearchOperation("AAAA"));
-//		sampleOperations.add(new SearchOperation("AA"));
-//		sampleOperations.add(new ExactSearchOperation("ACGT"));
-//		
-//		dnaTree.executeOperations(sampleOperations);
-		
 	}
 }
